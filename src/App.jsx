7d87b995138fa2499f1aa722875528d7f9e7a252
farrasazhary <farrasazhary17@gmail.com>
2024-11-30 22:06:46 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * @copyright 2024 Farras Azhary
+ * @license Apache-2.0
+ */
+
+/*
+ * Components
+*/
+import Header from "./components/Header";
+
+const App = () => {
+    return (
+        <Header />
+    )
+}
+
+export default App;

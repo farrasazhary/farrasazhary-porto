35dd0652de2460652eefb574c3c9455386eb46e8
farrasazhary <farrasazhary17@gmail.com>
2024-12-06 08:39:10 +0700

    done porto prototype

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+import 'lenis/dist/lenis.css'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+/*
+ * Register GSAP plugins
+*/
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
